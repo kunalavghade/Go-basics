deb40211b35bb9eae34f7221755fb0f93775c60f
Kunal Avghade <kunal.avghade@calfus.com>
2026-05-25 23:38:45 +0530

    even odd

---
+++ fundamental/even_odd.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	for _, num := range nums {
+		if num&1 != 0 {
+			fmt.Printf("%d is odd\n", num)
+		} else {
+			fmt.Printf("%d is even\n", num)
+		}
+
+	}
+}
